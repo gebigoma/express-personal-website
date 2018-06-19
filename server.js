c60f26abbb5f9f6acccdcb1d6d6e5482aec8261a
Tina <tparkmedia@gmail.com>
2018-06-19 12:42:35 -0700

    seperate router concerns

--- server.js
+++ server.js
@@ -2,30 +2,24 @@ const
   express = require('express'),
   app = express(),
   PORT = 3000,
-  fs = require('fs')
+  fs = require('fs'),
+  routes = require('./routes.js')
 
-const routes = (req, res) => {
-  const { method, url } = req;
-  fs.readFile('views/home.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end(); 
-  })
-}
 
- const handleRoot = (req, res) => {
-    res.send(`<h1>Hellooooo</h1>`)
-  }
-const handleAbout = (req, res) => {
-  res.send(`<h1>About ME!</h1>`)
-}
-const handleContact = (req, res) => {
-  res.send(`<h1>Call me, maybe</h1>`)
-}
 
-app.get('/', routes);
-app.get('/about', handleAbout);
-app.get('/contact', handleContact);
+//  const handleRoot = (req, res) => {
+//     res.send(`<h1>Hellooooo</h1>`)
+//   }
+// const handleAbout = (req, res) => {
+//   res.send(`<h1>About ME!</h1>`)
+// }
+// const handleContact = (req, res) => {
+//   res.send(`<h1>Call me, maybe</h1>`)
+// }
+
+app.get('/', routes.handleRoot);
+app.get('/about', routes.handleAbout);
+app.get('/contact', routes.handleContact);
 
 
 
