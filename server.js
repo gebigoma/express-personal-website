998c543fb67671486713f219516302b0156a6c2c
Tina <tparkmedia@gmail.com>
2018-06-19 12:12:23 -0700

    add express route to home

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const
   express = require('express'),
   app = express(),
-  PORT = 3000
+  PORT = 3000,
+  fs = require('fs')
 
-
-const handleRoot = (req, res) => {
-  res.send(`<h1>Hellooooo</h1>`)
+const routes = (req, res) => {
+  const { method, url } = req;
+  fs.readFile('views/home.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end(); 
+  })
 }
+
+ const handleRoot = (req, res) => {
+    res.send(`<h1>Hellooooo</h1>`)
+  }
 const handleAbout = (req, res) => {
   res.send(`<h1>About ME!</h1>`)
 }
@@ -14,7 +23,7 @@ const handleContact = (req, res) => {
   res.send(`<h1>Call me, maybe</h1>`)
 }
 
-app.get('/', handleRoot);
+app.get('/', routes);
 app.get('/about', handleAbout);
 app.get('/contact', handleContact);
 
